d462433fae5b352a9b885f1bd60975b9a0c8a811
银白色暴风雪 <a543055979@qq.com>
2017-04-24 12:22:17 +0800

    Vue2demo

---
+++ vue2demo/src/components/checkOrder.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <my-dialog :is-show="isShowCheckDialog" @on-close="checkStatus">
+      请检查你的支付状态！
+      <div class="button" @click="checkStatus">
+        支付成功
+      </div>
+      <div class="button" @click="checkStatus">
+        支付失败
+      </div>
+    </my-dialog>
+    <my-dialog :is-show="isShowSuccessDialog" @on-close="toOrderList">
+      购买成功！
+    </my-dialog>
+    <my-dialog :is-show="isShowFailDialog" @on-close="toOrderList">
+      购买失败！
+    </my-dialog>
+  </div>
+</template>
+
+<script>
+  import MyDialog from './base/dialog'
+  export default {
+    components: {
+      MyDialog
+    },
+    props: {
+      isShowCheckDialog: {
+        type: Boolean,
+        default: false
+      },
+      orderId: {
+        type: [String, Number]
+      }
+    },
+    data () {
+      return {
+        isShowSuccessDialog: false,
+        isShowFailDialog: false
+      }
+    },
+    methods: {
+      checkStatus () {
+        this.$http.post('/api/checkOrder', {
+          orderId: this.orderId
+        })
+          .then((res) => {
+            this.isShowSuccessDialog = true
+            this.$emit('on-close-check-dialog')
+          }, (err) => {
+//            this.isShowFailDialog = true
+//            this.$emit('on-close-check-dialog')
+            this.isShowSuccessDialog = true
+            this.$emit('on-close-check-dialog')
+          })
+      },
+      toOrderList () {
+        this.$router.push({path: '/orderList'})
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
